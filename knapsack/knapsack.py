528fc3297aa17f9306912edc5f63881ebc1f31a4
Marcus Grant <marcusfg@gmail.com>
2018-06-20 07:16:56 -0400

    add a few rpg items

---
+++ knapsack/knapsack.py
@@ -0,0 +1,27 @@
+treasure_chest = [
+    {
+        item: 'Book of Literacy',
+        value: 50,
+        weight: 1,
+    },
+    {
+        item: 'Magic Stick',
+        value: 100,
+        weight: 100,
+    },
+    {
+        item: 'Spoon of Stirring',
+        value: 20,
+        weight: 0.1,
+    },
+    {
+        item: 'The Tickler (+1 Strength Mace)',
+        value: 80,
+        weight: 10,
+    },
+    {
+        item: 'Saphire Ring',
+        value: 200,
+        weight: 0.1,
+    },
+]
